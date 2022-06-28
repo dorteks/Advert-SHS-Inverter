cd7e4ed99280422e7cb3cb0c3b25e82a632d2657
dorteks <ayodelealayemi@gmail.com>
2022-06-28 07:39:50 +0100

    progress

---
+++ function.js
@@ -0,0 +1,5 @@
+const moveHomayaImage = document.querySelector(".homaya .homayaimage");
+const moveSchneiderImage = document.querySelector(".schneider .schneiderimage");
+const movePhocosImage = document.querySelector(".phocos .phocosimage");
+
+moveHomayaImage.onkeyup = () => {};
